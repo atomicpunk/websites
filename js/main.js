14b33f37edabbe2c9efab3f80dd3d5a6d6644ce8
Todd Brandt <todd.e.brandt@intel.com>
2013-10-20 21:31:43 -0700

    turn off buttons if they won't fit on screen

--- js/main.js
+++ js/main.js
@@ -7,7 +7,15 @@ function Intro() {
     {
         var e = document.getElementById("main_page");
         e.className = "intro";
-    }
+        var myWidth = window.innerWidth;
+        var myHeight = window.innerHeight;
+		if(myWidth < 1300) {
+			var items = document.getElementsByClassName('enterdiv');
+			for (var i = 0; i < items.length; i++) {
+				items[i].style.display="none";
+			}
+		}
+	}
 
     init();
 }
